046ac55e326070d714d795c5ee06419830ab2f46
Daniel <gulnarikmihailova@gmail.com>
2025-06-18 13:38:36 +0300

    init 30_2 (добавление полей в сериализатор)

--- myproject/edu_materials/serializers.py
+++ myproject/edu_materials/serializers.py
@@ -4,9 +4,25 @@ from .models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    count_of_lessons = serializers.SerializerMethodField()
+    info_lessons = serializers.SerializerMethodField()
+
+    def get_count_of_lessons(self, obj):
+        return obj.lesson_set.count()
+
+    def get_info_lessons(self, obj):
+        lessons = obj.lesson_set.all()
+        return LessonSerializer(lessons, many=True).data
+
     class Meta:
         model = Course
-        fields = "__all__"
+        fields = (
+            "title",
+            "description",
+            "preview",
+            "count_of_lessons",
+            "info_lessons",
+        )
 
 
 class LessonSerializer(serializers.ModelSerializer):
